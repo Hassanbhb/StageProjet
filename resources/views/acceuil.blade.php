f9abec79f5b5425394db4b5ad5e7311c7f62c881
Hassanbhb <bussinesbhb@gmail.com>
2022-03-09 14:52:34 +0100

    added contact and footer

--- resources/views/acceuil.blade.php
+++ resources/views/acceuil.blade.php
@@ -1,143 +1,189 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Voyage</title>
+        <title>Voyage</title>
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href={{ URL::asset('css/app.css') }}>
-</head>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href={{ URL::asset('css/app.css') }}>
+    </head>
 
-<body>
-    <div class="header">
-        <div class="container">
-            <div class="navbar">
-                <a href="http://127.0.0.1:8000/"><img src="/img/logo.png" alt="logo" width="90px"></a>
-                <nav>
-                    <ul>
-                        <li><a href="">Acceuil</a></li>
-                        <li><a href="">Service</a></li>
-                        <li><a href="">Contact</a></li>
+    <body>
+        <div class="header">
+            <div class="container">
+                <div class="navbar">
+                    <a href="http://127.0.0.1:8000/"><img src="/img/logo.png" alt="logo" width="90px"></a>
+                    <nav>
+                        <ul>
+                            <li><a href="">Acceuil</a></li>
+                            <li><a href="">Service</a></li>
+                            <li><a href="">Contact</a></li>
 
-                        <li><button id="show-register">Inscription</button></li>
+                            <li><button id="show-register">Inscription</button></li>
 
-                    </ul>
-                </nav>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="popup" style="user-select: none;">
-        <div class="close-btn">×</div>
-        <div class="form">
-            <h2>Inscription</h2>
-            <div class="form-element">
-                <label for="email">Email</label>
-                <input type="text" id="email" placeholder="Entrer votre email">
-            </div>
-            <div class="form-element">
-                <label for="username">Nom d'utilisateur</label>
-                <input type="text" id="username" placeholder="Entrer votre nom d'utilisateur">
-            </div>
-            <div class="form-element">
-                <label for="password">Mot de passe</label>
-                <input type="password" id="password" placeholder="Entrer votre mot de passe">
-            </div>
-            <div class="form-element">
-                <label for="confirm-password">Confirmer le mot de passe</label>
-                <input type="password" id="password" placeholder="Confirmer votre mot de passe">
-            </div>
-            <div class="form-element">
-                <input type="checkbox" id="remember-me">
-                <label for="remember-me">Remember me</label>
-            </div>
-            <div class="form-element">
-                <button type="submit">Connexion</button>
-            </div>
+        <div class="popup">
+            <div class="close-btn">×</div>
+            <div class="form">
+                <h2>Inscription</h2>
+                <div class="form-element">
+                    <label for="email">Email</label>
+                    <input type="text" id="email" placeholder="Entrer votre email">
+                </div>
+                <div class="form-element">
+                    <label for="username">Nom d'utilisateur</label>
+                    <input type="text" id="username" placeholder="Entrer votre nom d'utilisateur">
+                </div>
+                <div class="form-element">
+                    <label for="password">Mot de passe</label>
+                    <input type="password" id="password" placeholder="Entrer votre mot de passe">
+                </div>
+                <div class="form-element">
+                    <label for="confirm-password">Confirmer le mot de passe</label>
+                    <input type="password" id="password" placeholder="Confirmer votre mot de passe">
+                </div>
+                <div class="form-element">
+                    <input type="checkbox" id="remember-me">
+                    <label for="remember-me">Remember me</label>
+                </div>
+                <div class="form-element">
+                    <button type="submit">Connexion</button>
+                </div>
 
-            <div class="form-element">
-                <button style="margin-bottom: 23px" id="show-login">Déja Inscrit?</button>
-                <a href="#" style="padding-left: 94px;">Mot passe oublié?</a>
+                <div class="form-element">
+                    <button style="margin-bottom: 23px" id="show-login">Déja Inscrit?</button>
+                    <a href="#" style="padding-left: 94px;">Mot passe oublié?</a>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="popup_login">
-        <div class="close-btn">×</div>
-        <div class="form">
-            <h2>Connexion</h2>
-            <div class="form-element">
-                <label for="email">Email</label>
-                <input type="text" id="email" placeholder="Entrer votre email">
-            </div>
-            <div class="form-element">
-                <label for="password">Mot de passe</label>
-                <input type="password" id="password" placeholder="Entrer votre mot de passe">
-            </div>
-            <div class="form-element">
-                <input type="checkbox" id="remember-me">
-                <label for="remember-me">Remember me</label>
-            </div>
-            <div class="form-element">
-                <button>Connexion</button>
-            </div>
-            <div class="form-element">
-                <a href="#">Mot passe oublié?</a>
+        <div class="popup_login">
+            <div class="close-btn">×</div>
+            <div class="form">
+                <h2>Connexion</h2>
+                <div class="form-element">
+                    <label for="email">Email</label>
+                    <input type="text" id="email" placeholder="Entrer votre email">
+                </div>
+                <div class="form-element">
+                    <label for="password">Mot de passe</label>
+                    <input type="password" id="password" placeholder="Entrer votre mot de passe">
+                </div>
+                <div class="form-element">
+                    <input type="checkbox" id="remember-me">
+                    <label for="remember-me">Remember me</label>
+                </div>
+                <div class="form-element">
+                    <button>Connexion</button>
+                </div>
+                <div class="form-element">
+                    <a href="#">Mot passe oublié?</a>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="welcome">
-        <div>
-            <p>Laissez-vous guider par votre curiosité</p>
-            <button>Explorez</button>
-        </div>
-    </div>
-    <div class="service">
-        <div class="s1">
+        <div class="welcome">
             <div>
-                <h1>Crée votre voyage</h1>
-                <p>Avec Saafr vous pouvez organizer votre experince depuis chez vous, votre annonce est affichée pour les gens du monde et ils peuvent vous rejoindre facilement.</p>
+                <p>Laissez-vous guider par votre curiosité</p>
+                <button>Explorez</button>
             </div>
-            <img class="s_svg" src={{ asset('images/tv-tg.svg') }} alt="" srcset="">
         </div>
-        <div class="s2">
-            <img class="s_svg" src= {{ asset('images/camp.svg') }} alt="A girl traveling">
+        <div class="service">
+            <div class="s1">
+                <div>
+                    <h1>Crée votre voyage</h1>
+                    <p>Avec Saafr vous pouvez organizer votre experince depuis chez vous, votre annonce est affichée pour les gens du monde et ils peuvent vous rejoindre facilement.</p>
+                </div>
+                <img class="s_svg" src={{ asset('images/tv-tg.svg') }} alt="" srcset="">
+            </div>
+            <div class="s2">
+                <img class="s_svg" src= {{ asset('images/camp.svg') }} alt="A girl traveling">
+                <div>
+                    <h1>Joindre un voyage</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, facilis mollitia vitae maiores at quasi maxime similique, iste, odio numquam nisi molestiae eius? Quas, fugit provident optio rerum perspiciatis asperiores!</p>
+                </div>
+            </div>
+        </div>
+        <div class="exp">
+            <h1>Les Experiences les plus populaire: </h1>
             <div>
-                <h1>Joindre un voyage</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, facilis mollitia vitae maiores at quasi maxime similique, iste, odio numquam nisi molestiae eius? Quas, fugit provident optio rerum perspiciatis asperiores!</p>
+                <div class="top">
+
+                </div>
+                <div class="stats">
+                    <div class="card">
+                        <p class="head">33</p>
+                        <p class="label">Expériences</p>
+                    </div>
+                    <div class="card">
+                        <p class="head">3000</p>
+                        <p class="label">Utilisateurs</p>
+                    </div>
+                    <div class="card">
+                        <p class="head">90%</p>
+                        <p class="label">satisfaction</p>
+                    </div>
+                </div>
             </div>
+
+
         </div>
-    </div>
-    <div class="exp">
-        <h1>Les Experiences les plus populaire: </h1>
 
+        <div class="contact">
+            <div class="con_info">
+                <span class="head">Contacter nous:</span>
+                <p><span>E-mail:</span> safir@gmail.com</p>
+                <p><span>Tél:</span> 06-73-34-32-05</p>
+                <a href="#"><img src={{ asset('images/face_logo.svg') }} alt="facebook logo"> Facebook</a>
+                <a href="#"><img src={{ asset('images/twitter_logo.svg') }} alt="twitter logo"> Twitter</a>
+                <a href="#"><img src={{ asset('images/insta_logo.svg') }} alt="instagram logo"> Instagram</a>
+            </div>
+            <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d215.0539107956635!2d-9.546908077789043!3d30.411640739983817!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xdb3b7da61b863c9%3A0x1134028ed4dab469!2sUnivers%20Startup%20et%20Entrepreneur%20-%20Incubateur%20et%20Espace%20Coworking%20Agadir!5e0!3m2!1sen!2sma!4v1646823925991!5m2!1sen!2sma&z=5" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
 
+        <footer>
+            <div class="big_logo">
+                <img src="#" alt="logo">
+            </div>
+            <div class="apps">
+                <p>Télécharger notre app sur Android et ios</p>
+                <div>
+                    <img src={{ asset("images/google_play.svg") }} alt="google play button">
+                    <img src={{ asset("images/ios_store.svg") }} alt="apple store button">
+                </div>
+            </div>
 
-    </div>
-    <script>
-        const inscription_btn = document.querySelector("#show-register");
-        const login_btn = document.querySelector("#show-login");
-        const close_btns = document.querySelectorAll(".close-btn");
+            <div class="copyright">
+                <p><img src={{ asset('images/copyright.svg') }} alt="coppyright"> 2022 Safir,inc </p>
+            </div>
+        </footer>
 
-        inscription_btn.addEventListener("click", function() {
-            document.querySelector(".popup").classList.add("active");
-        });
-        
-        login_btn.addEventListener("click", function() {
-            document.querySelector(".popup").classList.remove("active");
-            document.querySelector(".popup_login").classList.add("active");
-        });
+        <script>
+            const inscription_btn = document.querySelector("#show-register");
+            const login_btn = document.querySelector("#show-login");
+            const close_btns = document.querySelectorAll(".close-btn");
 
-        close_btns.forEach(btn => {
-            btn.addEventListener("click", (e) => {
-                e.target.parentElement.classList.remove("active");
-            })
-        });
-    </script>
+            inscription_btn.addEventListener("click", function() {
+                document.querySelector(".popup").classList.add("active");
+            });
+            
+            login_btn.addEventListener("click", function() {
+                document.querySelector(".popup").classList.remove("active");
+                document.querySelector(".popup_login").classList.add("active");
+            });
 
-
-</body>
+            close_btns.forEach(btn => {
+                btn.addEventListener("click", (e) => {
+                    e.target.parentElement.classList.remove("active");
+                })
+            });
+        </script>
+    </body>
 
 </html>
